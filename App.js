dce5c17be6ec0ae187b03e3afdaf0da8e05dae8a
faredistribuciones <faredistribuciones@gmail.com>
2025-01-31 13:17:09 -0300

    Deploy del proyecto corregido en GitHub Pages

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>CRM Vendedores</h1>
+            <p>¡Bienvenido a tu aplicación!</p>
+        </div>
+    );
+}
+
+export default App;
